2de6ea8ad626578567f9910839c9667af112ecd6
vanessanevesx <144918223+vanessanevesx@users.noreply.github.com>
2023-09-20 12:15:39 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,9 @@
+print("olá, mundo!")
+num1=float(input("Digite o primeiro número:"))
+num2=float(input("Digite o segundo número:"))
+soma=num1+ num2
+print(f"Soma:{soma}")
+nome=input("Insira seu nome")
+print(f'ola, {nome}')
+idade=int(input("digite seu ano de nascimento"))
+          
